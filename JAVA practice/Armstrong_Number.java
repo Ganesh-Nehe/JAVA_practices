bc4d457849eea7040fd7be0452ddb5395a3dd0dc
ganesh <ganeshnehe07@gmail.com>
2023-09-29 14:34:08 +0530

    update

--- JAVA practice/Armstrong_Number.java
+++ JAVA practice/Armstrong_Number.java
@@ -3,12 +3,27 @@ public class Armstrong_Number {
         int n=153;
         int temp=n,rem;
         int sum=0;
+        
+        int k=temp;
+        int count=0;
+        
+
+        while(temp!=0){
+            count++;
+            temp=temp/10;
+        }
         while(n!=0){
             rem = n%10;
-            sum = sum +(rem*rem*rem);
+            int i=1;
+            int t=1;
+            while(i<=count){
+                t=t*rem;
+                i++;
+            }
+            sum = sum + t;
             n=n/10;
         }
-        if(sum==temp){
+        if(sum==k){
             System.out.println("its a Armstrong number");
         }else{
             System.out.println("its not a armstrong number");
